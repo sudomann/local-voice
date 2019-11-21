ac0c8588e974aac714512e71bff083674768eb75
Willy Njundong <njundongw@gmail.com>
2019-11-21 05:06:13 -0500

    basic ui

--- mobile/main.js
+++ mobile/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View, SafeAreaView} from 'react-native';
 import {
   Layout,
   Button,
@@ -15,28 +15,63 @@ const HeartIcon = style => <Icon {...style} name="heart" />;
 export class Main extends React.Component {
   state = {
     selectedIndex: 0,
+    host: undefined,
+    port: undefined,
   };
 
-  onSelect = selectedIndex => {
+  onTabSelect = selectedIndex => {
     this.setState({selectedIndex});
+  };
+
+  onChangeHost = value => {
+    this.setState({host: value});
+  };
+  onChangePort = value => {
+    this.setState({port: value});
   };
   render() {
     return (
-      <Layout style={styles.container}>
-        <TabView
-          selectedIndex={this.state.selectedIndex}
-          onSelect={this.onSelect}>
-          <Tab title="HOST A VOICE CHAT">
-            <Text>HOST</Text>
-            <Button>Start Hosting</Button>
-          </Tab>
-          <Tab title="JOIN A VOICE CHAT">
-            <Text>JOIN</Text>
-            <Input />
-            <Input />
-            <Button>Join</Button>
-          </Tab>
-        </TabView>
+      <Layout style={{flex: 1}}>
+        <SafeAreaView>
+          <Layout style={[styles.container, styles.header]} level="1">
+            <Text category="h1" style={styles.text}>
+              Voice Chat Over Local Network
+            </Text>
+            <Text appearance="hint" style={styles.text}>
+              By Willy Njundong
+            </Text>
+          </Layout>
+          <Layout level="3" style={styles.tabContainer}>
+            <TabView
+              selectedIndex={this.state.selectedIndex}
+              onSelect={this.onTabSelect}>
+              <Tab title="HOST A VOICE CHAT">
+                <Layout level="4" style={styles.tabContainer}>
+                  <View style={styles.tabContent}>
+                    <Button>Start Hosting</Button>
+                  </View>
+                </Layout>
+              </Tab>
+              <Tab title="JOIN A VOICE CHAT">
+                <Layout level="4" style={styles.tabContainer}>
+                  <View style={styles.tabContent}>
+                    <Input
+                      label="HOST"
+                      value={this.state.host}
+                      onChangeText={this.onChangeHost}
+                    />
+                    <Input
+                      label="PORT"
+                      value={this.state.port}
+                      onChangeText={this.onChangePort}
+                    />
+                    <Button>Join</Button>
+                  </View>
+                </Layout>
+              </Tab>
+            </TabView>
+          </Layout>
+        </SafeAreaView>
       </Layout>
     );
   }
@@ -44,9 +79,18 @@ export class Main extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    margin: 10,
+  },
+  tabContainer: {
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
+  },
+  tabContent: {
+    margin: 30,
+    justifyContent: 'space-evenly',
+  },
+  header: {
+    marginBottom: 50,
   },
   text: {
     textAlign: 'center',
