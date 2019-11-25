890ad6dde0c2c1b5bb640e329ebf211e860bf306
Willy Njundong <njundongw@gmail.com>
2019-11-24 20:35:17 -0500

    added permissions and audio streaming functionality

--- mobile/main.js
+++ mobile/main.js
@@ -9,6 +9,7 @@ import {
   TabView,
   Input,
 } from 'react-native-ui-kitten';
+import TrackPlayer from 'react-native-track-player';
 
 const HeartIcon = style => <Icon {...style} name="heart" />;
 
@@ -17,11 +18,42 @@ export class Main extends React.Component {
     selectedIndex: 0,
     host: undefined,
     port: undefined,
+    mediaState: undefined,
   };
 
+  async componentDidMount() {
+    // Creates the player
+    TrackPlayer.setupPlayer().then(async () => {
+      // Adds a track to the queue
+      await TrackPlayer.add({
+        id: 'trackId',
+        url: 'http://192.168.1.134:9999/',
+        title: 'Voice Over Local Network',
+        artist: 'Willy Njundong',
+        album: 'Networking Project',
+        artwork:
+          'https://icon-library.net/images/voice-chat-icon/voice-chat-icon-2.jpg', // Load artwork from the network
+      });
+    });
+  }
+
+  componentWillUnmount() {}
   onTabSelect = selectedIndex => {
     this.setState({selectedIndex});
   };
+
+  getMediaState = async () => {
+    let state = await TrackPlayer.getState();
+    this.setState({mediaState: state});
+  };
+
+  onJoin = () => {
+    TrackPlayer.play();
+  };
+  onQuit = () => {
+    TrackPlayer.pause();
+  };
+  onStartHosting = () => {};
 
   onChangeHost = value => {
     this.setState({host: value});
@@ -37,21 +69,19 @@ export class Main extends React.Component {
             <Text category="h1" style={styles.text}>
               Voice Chat Over Local Network
             </Text>
+
             <Text appearance="hint" style={styles.text}>
               By Willy Njundong
             </Text>
+            <Text category="c1" style={styles.text}>
+              Current Media State: {this.state.mediaState}
+            </Text>
+            <Button onPress={this.getMediaState}>Update Media Status</Button>
           </Layout>
           <Layout level="3" style={styles.tabContainer}>
             <TabView
               selectedIndex={this.state.selectedIndex}
               onSelect={this.onTabSelect}>
-              <Tab title="HOST A VOICE CHAT">
-                <Layout level="4" style={styles.tabContainer}>
-                  <View style={styles.tabContent}>
-                    <Button>Start Hosting</Button>
-                  </View>
-                </Layout>
-              </Tab>
               <Tab title="JOIN A VOICE CHAT">
                 <Layout level="4" style={styles.tabContainer}>
                   <View style={styles.tabContent}>
@@ -65,7 +95,15 @@ export class Main extends React.Component {
                       value={this.state.port}
                       onChangeText={this.onChangePort}
                     />
-                    <Button>Join</Button>
+                    <Button onPress={this.onJoin}>Join</Button>
+                    <Button onPress={this.onQuit}>Quit</Button>
+                  </View>
+                </Layout>
+              </Tab>
+              <Tab title="HOST A VOICE CHAT">
+                <Layout level="4" style={styles.tabContainer}>
+                  <View style={styles.tabContent}>
+                    <Button onPress={this.onStartHosting}>Start Hosting</Button>
                   </View>
                 </Layout>
               </Tab>
