b8301a3b3a33c4999162ff4f518dc88a6d84eac7
Willy Njundong <njundongw@gmail.com>
2020-01-05 04:40:49 -0500

    saving RN work - partial functionality

--- mobile/main.js
+++ mobile/main.js
@@ -9,13 +9,13 @@ import {
   TabView,
   Input,
 } from 'react-native-ui-kitten';
-//import Audio from 'expo-av';
-import * as Network from 'expo-network';
+import {Audio} from 'expo-av';
 import * as FileSystem from 'expo-file-system';
+import * as Permissions from 'expo-permissions';
 import AudioRecord from 'react-native-audio-record';
 import {Buffer} from 'buffer';
 var dgram = require('react-native-udp');
-// const soundObject = new Audio.Sound();
+const soundObject = new Audio.Sound();
 
 const outputFileUri = '/storage/emulated/0/Download/' + 'localVoiceAudio';
 const options = {
@@ -25,59 +25,72 @@ const options = {
   //audioSource: 6, // android only (it seems this blocks ios)
   wavFile: 'test.wav', // default 'audio.wav'
 };
-var server = dgram.createSocket('udp4');
-var client = dgram.createSocket('udp4');
 
-server.on('error', function(msg, rinfo) {
-  console.log(rinfo, Buffer.from(msg));
-});
-
-server.on('message', async function(msg, remote) {
-  console.log(remote.address + ':' + remote.port + "'s message:" + msg);
-  const rcvdmsg = await FileSystem.writeAsStringAsync(outputFileUri, msg, {
-    encoding: FileSystem.EncodingType.Base64,
-  });
-  console.log('writeAsStringAsync() returned', rcvdmsg);
-});
-
-server.on('listening', function() {
-  let socketInfo = server.address();
-  console.log(
-    'UDP Server listening on ' + socketInfo.address + ':' + socketInfo.port,
-  );
-});
+const socketOptions = {
+  type: 'udp4',
+  //reuseAddr: true,
+};
+var server = dgram.createSocket(socketOptions);
+var client = dgram.createSocket(socketOptions);
+server.bind(12345);
+client.bind(12346);
 
 export class Main extends React.Component {
-  state = {
-    selectedIndex: 0,
-    serverStatus: 'Disconnected',
-    readyToHost: true,
-    host: undefined,
-    port: undefined,
-    deviceIpAddress: 'To be determined...',
-    socketPort: 'To be determined...',
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedIndex: 0,
+      serverStatus: 'Disconnected',
+      readyToHost: true,
+      host: undefined,
+      port: undefined,
+      deviceIpAddress: 'To be determined...',
+      socketPort: 'To be determined...',
+      recCount: 0,
+      msgCount: 0,
+    };
+
+    server.on('listening', () => {
+      const address = server.address();
+      console.log(`server listening ${address.address}:${address.port}`);
+    });
+
+    client.on('listening', () => {
+      const address = client.address();
+      console.log(`client listening ${address.address}:${address.port}`);
+      client.connect;
+    });
+  }
 
   async componentDidMount() {
-    AudioRecord.init(options);
-    AudioRecord.start();
-    AudioRecord.on('data', async data => {
-      //base64-encoded audio data chunks
+    // const {status} = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    server.on('message', async (msg, remote) => {
+      const decodedMsg = Buffer.from(msg).toString('base64');
+      this.setState({msgCount: this.state.msgCount + 1});
+      if (this.state.msgCount <= 1) {
+        console.log(
+          remote.address + ':' + remote.port + "'s plain message:" + msg,
+        );
+        console.log('decoded message:', decodedMsg);
+      }
+
       try {
-        await soundObject.loadAsync({
-          uri: `data:audio/mpeg;base64,${data}`,
-        });
-        await soundObject.playAsync();
-      } catch (error) {}
-      //let chunk = Buffer.from(data, 'base64');
-      //client.send(chunk, 0, chunk.length, 12345, '0.0.0.0');
-      //console.log('client just sent:', chunk);
+        const {sound: soundObject, status} = await Audio.Sound.createAsync(
+          {uri: `data:audio/wav;base64,${decodedMsg}`},
+          {shouldPlay: true},
+        );
+        // Your sound is playing!
+      } catch (error) {
+        console.log('ERROR when trying to play audio', error);
+      }
     });
+    AudioRecord.init(options);
   }
 
   componentWillUnmount() {}
   onTabSelect = selectedIndex => {
     this.setState({selectedIndex});
+    AudioRecord.stop();
   };
 
   onJoin = () => {};
@@ -87,8 +100,28 @@ export class Main extends React.Component {
       serverStatus: 'Initiallizing server...',
       readyToHost: false,
     });
+    try {
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        playsInSilentModeIOS: true,
+        shouldDuckAndroid: true,
+        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      });
+    } catch (error) {
+      console.log(error); // eslint-disable-line
+    }
+    AudioRecord.start();
 
-    server.bind(12345, '0.0.0.0');
+    AudioRecord.on('data', async data => {
+      this.setState({recCount: this.state.recCount + 1});
+      if (this.state.recCount <= 1) {
+        const chunk = Buffer.from(data, 'base64');
+        client.send(chunk, 0, chunk.length, 12345, '0.0.0.0');
+        console.log('client just sent chunk:', chunk);
+        console.log('client just sent data:', {data});
+      }
+    });
   };
 
   onChangeHost = value => {
@@ -178,4 +211,14 @@ const styles = StyleSheet.create({
   likeButton: {
     marginVertical: 16,
   },
+});
+
+server.on('error', err => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+client.on('error', err => {
+  console.log(`client error:\n${err.stack}`);
+  server.close();
 });
